e9289f417fe4bce1377237e94a9858a982874c3f
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-06-19 10:59:17 +0700

    Membuat unlimited loop untuk menulis dan berhenti apabila menulis karakter q

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -14,4 +14,13 @@ int main() {
 
 	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
+	// Unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// Loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		// Menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 }
