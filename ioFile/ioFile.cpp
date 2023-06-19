a447c83d72db2c0bef0148b46f385586d7ae6f91
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-06-19 10:57:11 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string baris;
+
+	// Membuka file dalam mode menulis.
+	ofstream outfile;
+	// Menunjuk ke sebuah nama file.
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+}
