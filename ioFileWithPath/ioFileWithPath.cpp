66033f49ed04318b9f2ef83db61d5739101d32c1
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-06-19 11:14:04 +0700

    Membuat unlimited loop dan akan berhenti apabila memasukan kerakter q

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -18,4 +18,14 @@ int main() {
 
 	cout << ">= menulis file, \'q\' untuk keluar" << endl;
 
+	// Unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// Mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// Loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		// Menulis dan Memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
 }
